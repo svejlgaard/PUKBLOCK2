d08bc0320bd6032a485d5ac58b75f96fab869e00
Simone <svejlgaard@protonmail.com>
2021-01-12 15:52:47 +0100

    trying 20 with time label

--- Total/Data_Visualisation.py
+++ Total/Data_Visualisation.py
@@ -452,7 +452,7 @@ class Combination():
 
             cluster_frame.insert(0, 'Name', pd.Series(final_names), True)
             print(f'cluster_{cluster}_p{self.perplexity}_G{self.magnitude}.csv')
-            cluster_frame.to_csv(f'cluster_{cluster}_p{self.perplexity}_G{self.magnitude}.csv')
+            cluster_frame.to_csv(f'cluster_{time_signature}_{cluster}_p{self.perplexity}_G{self.magnitude}.csv')
 
         self.clustered_dict = clustered_dict
 
